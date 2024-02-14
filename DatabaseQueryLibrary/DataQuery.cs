cd696e1a2ce23b8f4433b63841c32d6a2947de2c
MoltenSlag <MoltenSlag@users.noreply.github.com>
2024-02-14 18:18:58 -0500

    Added library for assignment
    DatabaseQueryLibrary is Alex Gann's library for the assignment.

---
+++ DatabaseQueryLibrary/DataQuery.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseQueryLibrary
+{
+    public class DataQuery
+    {
+        private StateDataClassesDataContext db;
+
+        public DataQuery()
+        {
+            db = new StateDataClassesDataContext();
+        }
+
+        public IQueryable<string> GetStateNames()
+        {
+            /* This query generates an IQueryable object full of state names
+             from the database and returns it. */
+
+            IQueryable<string> results = from state in db.States
+                                         select state.StateName;
+            return results;
+        }
+
+        public State GetRowByState(string row)
+        {
+            /* This query finds a State object, representing an entire
+             row of the table, based on the stateName. */
+            var results = from state in db.States
+                          where state.StateName == row
+                          select state;
+            return results.FirstOrDefault();
+        }
+    }
+}
